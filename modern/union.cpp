a477d7eb8c9887e1dcb8902d3008dd06a6985f4e
Chlorine <yoghurtlee123@gmail.com>
2024-05-07 16:20:55 +0800

    backup 2024-05-07

---
+++ modern/union.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdint>
+
+union union_name
+{
+    int i;
+    char c;
+};
+
+
+int main()
+{
+    union_name u;
+    u.i = 1;
+
+    std::cout << u.i << std::endl;
+    u.c = 'a';
+    std::cout << u.c << std::endl;
+    std::cout << u.i << std::endl;
+
+    int *pi = &u.i;
+    char *pc = &u.c;
+    std::cout << pi << std::endl;
+    std::cout << static_cast<void *>(pc) << std::endl;
+
+    return 0;
+}
